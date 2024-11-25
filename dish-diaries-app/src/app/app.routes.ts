c095ed5e0a45720f80bc7b9c5e962e14e09c8b23
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-11-25 13:34:34 +0200

    Add register component

--- dish-diaries-app/src/app/app.routes.ts
+++ dish-diaries-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
