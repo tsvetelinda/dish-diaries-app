f1e117eff5b1e650024ca26e4ea755cc1047ced8
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-11-26 10:57:39 +0200

    Update the styling of the catalog

--- dish-diaries-app/src/app/app.routes.ts
+++ dish-diaries-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { ListComponent } from './dish/list/list.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: 'list', component: ListComponent},
 ];
