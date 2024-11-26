66d46ffd9344becce67563c0d6bbaa1d17467a9c
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-11-26 13:34:39 +0200

    Update details component styling

--- dish-diaries-app/src/app/app.routes.ts
+++ dish-diaries-app/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ListComponent } from './dish/list/list.component';
+import { DetailsComponent } from './dish/details/details.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'list', component: ListComponent},
+    {path: 'list/:id', component: DetailsComponent}
 ];
--- dish-diaries-app/src/app/dish/list/list.component.ts
+++ dish-diaries-app/src/app/dish/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Dish } from '../../types/dish';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
