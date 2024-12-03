0f90a4417a726aac7fe3307803aeba5b52c581a4
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-12-03 15:22:09 +0200

    Include Add Dish component

--- dish-diaries-app/src/app/app.routes.ts
+++ dish-diaries-app/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ListComponent } from './dish/list/list.component';
 import { DetailsComponent } from './dish/details/details.component';
+import { AddComponent } from './dish/add/add.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'list', component: ListComponent},
-    {path: 'list/:id', component: DetailsComponent}
+    {path: 'list/:id', component: DetailsComponent},
+    {path: 'add', component: AddComponent}
 ];
