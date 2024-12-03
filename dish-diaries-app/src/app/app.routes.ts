074209ae00f65d661f30de5f9f84f5680a79ffdf
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-12-03 22:12:20 +0200

    Add Surprise Dish component

--- dish-diaries-app/src/app/app.routes.ts
+++ dish-diaries-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './user/register/register.component';
 import { ListComponent } from './dish/list/list.component';
 import { DetailsComponent } from './dish/details/details.component';
 import { AddComponent } from './dish/add/add.component';
+import { SurpriseComponent } from './dish/surprise/surprise.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path: 'register', component: RegisterComponent},
     {path: 'list', component: ListComponent},
     {path: 'list/:id', component: DetailsComponent},
-    {path: 'add', component: AddComponent}
+    {path: 'add', component: AddComponent},
+    {path: 'surprise', component: SurpriseComponent}
 ];
---
+++ dish-diaries-app/src/app/dish/surprise/surprise.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { SingleCardComponent } from '../single-card/single-card.component';
+import { Dish } from '../../types/dish';
+import { ApiService } from '../../api.service';
+
+@Component({
+  selector: 'app-surprise',
+  standalone: true,
+  imports: [SingleCardComponent],
+  templateUrl: './surprise.component.html',
+  styleUrl: './surprise.component.css'
+})
+export class SurpriseComponent implements OnInit {
+  randomDish : Dish | undefined = undefined;
+  dishes: Dish[] = [];
+  randomIndex: number = 0;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getDishes().subscribe(dishes => {
+      this.dishes = dishes;
+
+      this.randomIndex = Math.floor(Math.random() * dishes.length);
+      this.randomDish = this.dishes[this.randomIndex];
+    })
+  }
+}
