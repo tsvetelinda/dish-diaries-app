02bb2a27f35e20fc796e0dd6d3e936e6ca97691c
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-12-03 18:13:38 +0200

    Add field validations and error messages to the page

--- dish-diaries-app/src/app/dish/add/add.component.ts
+++ dish-diaries-app/src/app/dish/add/add.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.css'
 })
 export class AddComponent {
+  isFieldEmpty(controlName: NgModel) {
+    return controlName?.touched && controlName?.errors?.['required'];
+  }
 
+  isMinLengthValid(controlName: NgModel) {
+    return controlName?.touched && controlName?.errors?.['minlength'];
+  }
+
+  addDish(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(form);
+  }
 }
