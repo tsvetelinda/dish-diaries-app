6ae28974287701d3a5e1009479c4316e841dde49
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-12-10 10:43:09 +0200

    Add conditional showing for the Tried it button

--- dish-diaries-app/src/app/dish/details/details.component.ts
+++ dish-diaries-app/src/app/dish/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../../types/dish';
 import { ApiService } from '../../api.service';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-details',
@@ -13,7 +14,11 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent implements OnInit {
   dish = {} as Dish;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService, private userService: UserService) { }
+
+  get isLoggedIn() : boolean {
+    return this.userService.isLogged;
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
