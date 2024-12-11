9321302328a15bc6f29566bf1d3e48d799c5fb7c
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2024-12-11 21:07:25 +0200

    Update the user service

--- dish-diaries-app/src/app/user/user.service.ts
+++ dish-diaries-app/src/app/user/user.service.ts
@@ -1,55 +1,73 @@
-import { HttpClient } from '@angular/common/http';
+/*import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, tap } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
+})*/
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root',
 })
 export class UserService {
-  private user$$ = new BehaviorSubject<User | null>(null);
-  private user$ = this.user$$.asObservable();
-
-  USER_KEY = '[user]';
-  user: User | null = null;
-
-  get isLogged(): boolean {
-    return !!this.user;
-  }
-
-  constructor(private http: HttpClient) {
-    this.user$.subscribe((user) => {
-      this.user = user;
-    });
-   }
+  constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
-    return this.http.post<User>('/api/users/login', { email, password })
-    .pipe(tap((user) => this.user$$.next(user)));
+    return this.http.post<User>('/api/users/login', { email, password }).pipe(
+      tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+      })
+    );
   }
 
   logout() {
-    return this.http.post('/api/users/logout', {})
-    .pipe(tap((user) => this.user$$.next(null)));
+    return this.http.post('/api/users/logout', {}).pipe(
+      tap(() => {
+        localStorage.removeItem('user');
+      })
+    );
   }
 
   register(email: string, password: string, chefName: string, favCuisine: string, cookingSkillLevel: string) {
-    return this.http.post<User>('/api/users/register', { email, password, chefName, favCuisine, cookingSkillLevel})
-    .pipe(
-      tap((user) => this.user$$.next(user))
+    return this.http.post<User>('/api/users/register', { email, password, chefName, favCuisine, cookingSkillLevel }).pipe(
+      tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+      })
     );
   }
 
   editProfile(userId: string | undefined, email: string, favCuisine: string, cookingSkillLevel: string) {
-    return this.http.put<User>(`/api/users/${userId}`, { email, favCuisine, cookingSkillLevel });
+    return this.http.put<User>(`/api/users/${userId}`, { email, favCuisine, cookingSkillLevel }).pipe(
+      tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+      })
+    );
   }
 
   getProfile() {
-    return this.http.get<User>('/api/users/profile')
-    .pipe(tap((user) => this.user$$.next(user)));
+    return this.http.get<User>('/api/users/profile').pipe(
+      tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+      })
+    );
   }
 
-  getUserId() {
-    return this.user$$.value?._id;
+  getUserId(): string | undefined {
+    const user = this.getUserFromLocalStorage();
+    return user ? user._id : undefined;
+  }
+
+  private getUserFromLocalStorage(): User | null {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  }
+
+  get isLogged(): boolean {
+    return !!this.getUserFromLocalStorage();
   }
 }
