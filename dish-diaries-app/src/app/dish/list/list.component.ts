9a7463569b8f7a1382c93a8af0d6f195626a8ea5
Tsvetelinda <56298370+tsvetelinda@users.noreply.github.com>
2025-02-10 11:59:50 +0200

    Update the loader's design

--- dish-diaries-app/src/app/dish/list/list.component.ts
+++ dish-diaries-app/src/app/dish/list/list.component.ts
@@ -3,11 +3,12 @@ import { ApiService } from '../../api.service';
 import { Dish } from '../../types/dish';
 import { SingleCardComponent } from '../single-card/single-card.component';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [SingleCardComponent],
+  imports: [SingleCardComponent, LoaderComponent],
   templateUrl: './list.component.html', 
   styleUrl: './list.component.css',
   animations: [
@@ -25,12 +26,14 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 }) 
 export class ListComponent implements OnInit {
   dishes: Dish[] = [];
+  isLoading = true;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getDishes().subscribe(dishes => {
       this.dishes = dishes;
+      this.isLoading = false;
     });
   }
 }
